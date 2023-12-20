0eb6ebdd6ae0370065f8a55cd417f0889fa10b32
GiovanniLafortezza <recordeknot388@gmail.com>
2023-12-20 14:21:46 +0100

    add : step 2

--- db_university_query.sql
+++ db_university_query.sql
@@ -6,3 +6,9 @@ order by surname asc;
 select *
 from teachers t 
 order by name asc;
+
+
+-- Selezionare tutti gli insegnamenti (courses) che valgono almeno 12 crediti (cfu) (375)
+select *
+from courses c 
+where c.cfu >= 12;
