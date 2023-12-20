c2451c808daa03b71f89cfea45c67166944bbb99
GiovanniLafortezza <recordeknot388@gmail.com>
2023-12-20 14:18:40 +0100

    add : first step

---
+++ db_university_query.sql
@@ -0,0 +1,8 @@
+-- Selezionare cognome e nome tutti gli insegnanti, ordinati per cognome e poi per nome in ordine alfabetico (100)
+select *
+from teachers t 
+order by surname asc;
+
+select *
+from teachers t 
+order by name asc;
